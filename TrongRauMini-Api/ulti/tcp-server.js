f1f36cce96845a554572d55ca3917f7fe0668596
caonhungoc <caonhungoc1996@gmail.com>
2021-02-25 23:03:22 +0700

    unique device id

--- TrongRauMini-Api/ulti/tcp-server.js
+++ TrongRauMini-Api/ulti/tcp-server.js
@@ -27,32 +27,36 @@ server.on('connection', socket => {
 
     socket.on('data', data => {
         if (!sockets[socket.id]) {
-            socket.deviceId = data.toString().trim();
-            socket.connectTime = new Date();
-            //socket.write(`Welcome ${socket.deviceId}!\n`);
-            sockets[socket.id] = socket;
+            const key = data.slice(data.toString().indexOf('.') + 1);
+            const id = data.slice(0, data.toString().indexOf('.'));
+            if('pdnc' === key) {
+                socket.deviceId = id; // data.toString().trim();
+                socket.connectTime = new Date();
+                //socket.write(`Welcome ${socket.deviceId}!\n`);
+                sockets[socket.id] = socket;
 
-            let countSocket = [], i = 0;
-            Object.entries(sockets).forEach(([key, sc]) => {
-                console.log(`${socket.id}: ${data}`)
-                if (socket.deviceId === sc.deviceId) {
-                    countSocket[i++]  = sc;
-                    if(i == 2) {
-                        if(countSocket[0].connectTime.getTime() < countSocket[1].connectTime.getTime()) {
-                            countSocket[0].destroy();
-                            delete sockets[countSocket[0].id];
-                            console.log('close old connection!');
-                        }
-                        else {
-                            countSocket[1].destroy();
-                            delete sockets[countSocket[1].id];
-                            console.log('close old connection!');
+                let countSocket = [], i = 0;
+                Object.entries(sockets).forEach(([key, sc]) => {
+                    console.log(`${socket.id}: ${data}`)
+                    if (socket.deviceId === sc.deviceId) {
+                        countSocket[i++]  = sc;
+                        if(i == 2) {
+                            if(countSocket[0].connectTime.getTime() < countSocket[1].connectTime.getTime()) {
+                                countSocket[0].destroy();
+                                delete sockets[countSocket[0].id];
+                                console.log('close old connection!');
+                            }
+                            else {
+                                countSocket[1].destroy();
+                                delete sockets[countSocket[1].id];
+                                console.log('close old connection!');
+                            }
                         }
                     }
-                }
-            })
+                })
 
-            return;
+                return;
+            }
         }
 
         Object.entries(sockets).forEach(([key, sc]) => {
